d2337cd4cdf52728866f3a580819adabd645cfd3
Ryan Silva <r.silva.webdev@gmail.com>
2015-05-28 16:17:29 -0700

    created a closure inside of plugin function that handles the lazy loading functionality

--- jquery.halfass.js
+++ jquery.halfass.js
@@ -19,12 +19,10 @@
 
         var opts = $.extend({}, $.fn.halfass.defaults, options);
         var elements = this;
+        var win = $(window);
         var i;
         var len;
-
-        function watcher() {
-            
-        }
+        var halfAss;
 
         // Set placeholder image on half ass images
         for(i = 0, len = elements.length; i < len; i++) {
@@ -37,14 +35,99 @@
 
         }
 
+        function HalfAss() {
+
+            var instance = this;
+            var imagesLoaded = false;
+
+            function initialize() {
+
+                // listener
+                instance.listeners();
+
+            };
+
+            this.listeners = function() {
+
+                // If all images have not loaded
+                if(imagesLoaded === false) {
+
+                    $(window)
+                        .on({
+                            scroll: instance.elementLoop
+                        });
+
+                }
+
+            };
+
+            this.elementLoop = function() {
+
+                console.log(imagesLoaded);
+
+                for(i = 0, len = elements.length; i < len; i++) {
+
+                    instance.inViewport($(elements[i]));
+
+                }
+
+            };
+
+            this.inViewport = function(e) {
+
+                var winHeight = window.innerHeight ? window.innerHeight : win.height()
+                var imageOffset = e.offset().top;
+                var fold = (imageOffset - winHeight) - opts.threshold;
+                var ogSrc = e.data(opts.dataAttribute);
+
+                // Window scroll check
+                if(win.scrollTop() >=  fold) {
+
+                    // Load image
+                    e.attr("src", ogSrc);
+
+                    // Check to see if all
+                    // images have loaded
+                    instance.loadingComplete();
+                    
+                }
+
+            };
+
+            this.loadingComplete = function(e) {
+
+                var el = $(".half-ass").last();
+
+                // All images loaded
+                if(el.attr("src") === el.data(opts.dataAttribute)) {
+
+                    // Stop scroll even from firing
+                    $(window).unbind('scroll');
+
+                    // Images loaded
+                    imagesLoaded = true;
+                }
+
+            };
+
+            initialize();
+
+        };
+
+        $(document).ready(function() {
+            halfAss = new HalfAss();
+        });
+
         return this;
     };
 
     $.fn.halfass.defaults = {
-        effect          : "fade",
-        loadAction      : "pageLoad"
+        effect          : "none",
+        threshold       : 0,
+        loadAction      : "pageLoad",
         dataAttribute   : "original",
-        placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+        placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC",
+        afterLoaded     : function() {}
     };
  
 }(jQuery, window, document));
